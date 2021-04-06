618713bacc2c80a5e03dd19ba509783231982d4e
Takahiko Wada <wada.takahiko@gmail.com>
2021-04-06 21:08:44 +0900

    FlatListを使う

--- App.js
+++ App.js
@@ -1,30 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import { ListItem } from './components/ListItem';
+
+const ARTICLES = [
+  {
+    urlToImage: 'https://picsum.photos/200',
+    title:
+      'React Native は、Facebookが開発したクロスプラットフォームアプリ開発用フレームワークです。 ReactNative でアプリを作ることで、クロスプラットフォーム( iOS / Androidどちらでも動く)アプリを作ることができます。',
+    author: 'Techニュース',
+  },
+  {
+    urlToImage: 'https://picsum.photos/201',
+    title:
+      'React Native Japan コミュニティは、日本でのReact Nativeの普及のため、活動を行っているコミュニティです。',
+    author: 'React Native Japan',
+  },
+  {
+    urlToImage: 'https://picsum.photos/203',
+    title:
+      'JavaScriptはプロトタイプベースのオブジェクト指向スクリプト言語であるが、クラスなどのクラスベースに見られる機能も取り込んでいます。',
+    author: 'JavaScriptFan',
+  },
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ListItem
-        imageUrl="https://picsum.photos/200"
-        title="React Native は、Facebookが開発したクロスプラットフォームアプリ開発用フレームワークです。 ReactNative でアプリを作ることで、クロスプラットフォーム( iOS / Androidどちらでも動く)アプリを作ることができます。"
-        subTitle="Techニュース"
-      />
-      <ListItem
-        imageUrl="https://picsum.photos/201"
-        title="React Native Japan コミュニティは、日本でのReact Nativeの普及のため、活動を行っているコミュニティです。"
-        subTitle="React Native Japan"
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={ARTICLES}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            subTitle={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
